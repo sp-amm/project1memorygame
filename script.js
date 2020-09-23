d91f3610a9a2895895055c05dde18b62d48ebab3
sp <sam.pack.sp@gmail.com>
2020-09-23 21:15:33 +0930

    pseudo coded basic outline of required functions

---
+++ script.js
@@ -0,0 +1,26 @@
+//Pseudocode
+
+//Easy- 10 cards w/Timer counting up
+//Meduim- 16 cards w/Timer counting up 
+//Hard- 22 cards w/Timer counting up
+//beat the clock
+
+//on click, hide menu
+
+//set gameboard
+    //start timer
+    //call API to card fronts
+    //store in cache/local storage
+    //using each image twice
+    //asign gifs at random
+
+//gameplay    
+    //count moves
+    //score displayed
+    //cards clicked
+    //two clicks
+    //if cards = same then hide +2 score (Reset clicks)
+    //if else cards = different -1 flip back (Reset clicks)
+    //all cards hidden then end game
+    //show score + remaining time
+    //button restart + return to menu
